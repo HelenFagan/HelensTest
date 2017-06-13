1ff2f1197f27d82bd689f4d90cbe2b5ae21304e8
HelenFagan <Helen.Fagan@cit.ie>
2017-06-13 14:28:32 +0100

    made changes

--- src/sample.py
+++ src/sample.py
@@ -1,9 +1,11 @@
 def power(x,y=2):
    result = x
-   for i in range(0,y):
-	result = result * x
-   return result
+   return x ** y
+ #  for i in range(0,y):
+#	result = result * x
+ #  return result
 
 a = 10
 b = 5
 print ('the result is ' , power(10,5))
+print ("The End")
